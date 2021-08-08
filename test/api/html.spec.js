209803f904dede95a7812371b8f1a8046630e536
“dananboom” <2907355904@qq.com>
2021-08-09 00:21:57 +0800

    test: api/html

---
+++ test/api/html.spec.js
@@ -0,0 +1,30 @@
+import {expect} from 'chai'
+import $ from '../../src/index.js'
+
+describe('Get the html', function() {
+    before(function() {
+        document.body.innerHTML = '<div>hello jquery-es</div>'
+    })
+    it('Get html of body', function() {
+        expect($('body').html()).to.be.equal('<div>hello jquery-es</div>')
+    })
+
+    it('Get html of div', function() {
+        expect($('div').html()).to.be.equal('hello jquery-es')
+    })
+})
+
+describe('Set the html', function() {
+    before(function() {
+        document.body.innerHTML = '<div></div>'
+    })
+    it('Set', function() {
+        $('div').html('hello jquery-es')
+        expect($('div').html()).to.be.equal('hello jquery-es')
+    })
+
+    it('Chain set', function() {
+        $('div').html('hello').html('jquery-es')
+        expect($('div').html()).to.be.equal('jquery-es')
+    })
+})
